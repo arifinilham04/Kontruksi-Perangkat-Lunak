ebbf00873d40b1a9f9f284f9dd575eedde7e44f6
arifin <86230082+arifinilham04@users.noreply.github.com>
2024-03-12 22:30:39 +0700

    Add files via upload

---
+++ TPMOD4/TPMOD4/Program.cs
@@ -0,0 +1,26 @@
+﻿using TPMOD4.kodepos;
+using TPMOD4.kodepos2;
+using TPMOD4.door;
+using System;
+
+namespace TPMOD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contoh_1 :");
+            KodePos kodepos = new KodePos();
+            kodepos.setKodePost();
+            kodepos.getKodePos();
+
+            Console.WriteLine("\nContoh_2 :");
+            KodePos1 kodepos1 = new KodePos1();
+            kodepos1.getKodePos();
+
+            Console.WriteLine("\nSoal 3 :");
+            DoorMachine door = new DoorMachine();
+            door.run();
+        }
+    }
+}
